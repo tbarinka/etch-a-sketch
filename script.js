884febfd1251a0d5f777ca42b2b559a7a7d1ca75
Taylor Barinka <barinkataylor@gmail.com>
2022-07-14 12:23:13 -0400

    create 16x16 grid with hover effect

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const container = document.getElementById('container');
+let container = document.getElementById('container');
+
+//code that executes function to create 16 divs
+function createDivs(number) {
+    for (let i = 1; i <= number; i++) {
+        let cell = document.createElement('div');
+        cell.id = "cell-" + i;
+        cell.className = "cell";
+        cell.textContent = "   .    "
+        container.appendChild(cell);
+    }
+} createDivs(256);
